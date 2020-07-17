6dd36ac2967892590736d15c99a53e25f4a6ecc6
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-17 10:50:06 -0700

    mvp

--- names/names.py
+++ names/names.py
@@ -2,21 +2,80 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # Case 1: value is less than self.value
+        if value < self.value:
+            # If there is no left child, insert value here
+            if self.left is None:
+               self.left = BSTNode(value)
+            else:
+                # Repeat the process on left subtree
+                self.left.insert(value)
+
+        # Case 2: value is greater than or equal self.value
+        elif value >= self.value:
+            # If there is no right child, insert value here
+            if self.right is None:
+               self.right = BSTNode(value)
+            else:
+                # Repeat the process on right subtree
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # Case 1: self.value is equal to the target
+        if self.value == target:
+            return True
+        # Case 2: target is less than self.value 
+        if target < self.value:
+            # if self.left is None, it isn't in the tree
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        # Case 3: otherwise
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+# make a binary search tree for names_1.py and set the first node as the root
+names_search_tree = BSTNode(names_1[0])
+# lopp through every single name in names_1.py
+for name in names_1:
+    # insert each name into the binary search tree
+    names_search_tree.insert(name)
+# loop through every single name in names_2.py
+for name in names_2:
+    # if the name already exists in the binary tree
+    if names_search_tree.contains(name):
+        # add the name to the duplicates list
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.storage = []
+        self.oldest_index = 0
+    
     def append(self, item):
-        pass
+        # if the buffer storage is not full
+        if len(self.storage) < self.capacity:
+            # we can add item to it
+            self.storage.append(item)
+        # if the buffer storage if full
+        else:
+            # the newly added item will replace the oldest item in the buffer storage
+            self.storage[self.oldest_index] = item
+            # the item to the right then become the oldest
+            self.oldest_index +=1
+
+        # if the oldest item index reaches the capacity
+        if self.oldest_index is self.capacity:
+            # the oldest index will be 0 again
+            self.oldest_index = 0      
 
     def get(self):
-        pass
+        return self.storage
